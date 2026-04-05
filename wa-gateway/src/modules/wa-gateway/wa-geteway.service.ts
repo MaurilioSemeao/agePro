0f5fd53b43b541658792012430370bb1728d2d26
maurilio <maurilio.ssemeao@gmail.com>
2026-04-05 20:49:29 -0300

    feat: integração WA Gateway com whatsapp-web.js e controle de sessões

--- wa-gateway/src/modules/wa-gateway/wa-geteway.service.ts
+++ wa-gateway/src/modules/wa-gateway/wa-geteway.service.ts
@@ -10,30 +10,48 @@ import {
 
 export class WaSessionService{
     private _sessions: Map<string, Client> = new Map();
+    private _sessionStatus: Map<string, boolean> = new Map();
 
     public createSession (payload: CreateSessionRequest): SessionStatusResponse{
+
+        if(this._sessions.has(payload.name)) {
+            throw new Error(`Session with name ${payload.name} already exists`);
+        }
+
         const client = new Client({
             authStrategy: new LocalAuth({clientId: payload.name}),
             puppeteer:{headless: true},
         });
         client.initialize();
+
         this._sessions.set(payload.name, client);
+        this._sessionStatus.set(payload.name, false);
+
         client.on('qr', (qr) => {
             console.log('scan this QR:');
-            qrcode.generate(qr, { small: true });
+            qrcode.generate(qr, { small: true});
         });
-        client.on('ready', () => console.log(`Session ${payload.name} ready`));
+        client.on('ready', () => {
+            console.log(`Session ${payload.name} ready`)
+            this._sessionStatus.set(payload.name, true);
+        });
+        client.on('authenticated', () => console.log(`Session ${payload.name} authenticated`));
+        client.on('auth_failure', () => console.error(`Session ${payload.name} auth failure:`));
+        client.on('disconnected', () => {
+            console.log(`Session ${payload.name} disconnected`);
+            this._sessionStatus.set(payload.name, false);
+        });
         return { id: payload.name, connected: false };
     };
 
 
     public async getSessionStatus(sessionId: string):Promise<SessionStatusResponse>{
         const client = this._sessions.get(sessionId);
-        const state = client ? await client.getState() : null;
+        const connected = this._sessionStatus.get(sessionId) || false;
 
         return {
             id: sessionId,
-            connected: state === 'CONNECTED',
+            connected,
             qr: client ? undefined : 'Session not found',
         }
     }
